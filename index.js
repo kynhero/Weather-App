16caa85b89ccee5bdd63f9d86861055c952f0aea
Kyn <kynhero@gmail.com>
2023-08-28 15:46:07 +0700

    Add SearchClick when Enter

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
 
+// Tự động click khi ấn phím Enter
+let myText = document.getElementById("my-text");
+let btn = document.getElementById("btn");
+
+myText.addEventListener("keyup", e => {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+        btn.click();
+    }
+});
+
 // Thêm sự kiện click cho nút tìm kiếm
 search.addEventListener('click', () => {
 
@@ -18,64 +29,66 @@ search.addEventListener('click', () => {
 
     // Gửi yêu cầu đến API của OpenWeatherMap với URL được xây dựng từ giá trị của thành phố và khóa API
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
-    .then(response => response.json())
-    .then(json => {
+        .then(response => response.json())
+        .then(json => {
 
-        // Nếu mã trả về là '404', hiển thị thông báo lỗi 404 và ẩn các hộp chi tiết thời tiết
-        if (json.cod === '404') {
-            container.style.height = '400px';
-            weatherBox.style.display = 'none';
-            weatherDetails.style.display = 'none';
-            error404.style.display = 'block';
-            error404.classList.add('fadeIn');
-            return;
-        }
+            // Nếu mã trả về là '404', hiển thị thông báo lỗi 404 và ẩn các hộp chi tiết thời tiết
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
 
-        // Ẩn thông báo lỗi 404 và hiển thị các hộp chi tiết thời tiết
-        error404.style.display = 'none';
-        error404.classList.remove('fadeIn');
+            // Ẩn thông báo lỗi 404 và hiển thị các hộp chi tiết thời tiết
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
 
-        // Cập nhật các giá trị cho các phần tử HTML như hình ảnh, nhiệt độ, mô tả, độ ẩm và tốc độ gió
-        const image = document.querySelector('.weather-box img');
-        const temperature = document.querySelector('.weather-box .temperature');
-        const description = document.querySelector('.weather-box .description');
-        const humidity = document.querySelector('.weather-details .humidity span');
-        const wind = document.querySelector('.weather-details .wind span');
+            // Cập nhật các giá trị cho các phần tử HTML như hình ảnh, nhiệt độ, mô tả, độ ẩm và tốc độ gió
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
 
-        switch (json.weather[0].main) {
-            case 'Clear':
-                image.src = 'images/clear.png';
-                break;
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
 
-            case 'Rain':
-                image.src = 'images/rain.png';
-                break;
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
 
-            case 'Snow':
-                image.src = 'images/snow.png';
-                break;
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
 
-            case 'Clouds':
-                image.src = 'images/cloud.png';
-                break;
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
 
-            case 'Haze':
-                image.src = 'images/mist.png';
-                break;
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
 
-            default:
-                image.src = '';
-        }
+                default:
+                    image.src = '';
+            }
 
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-        description.innerHTML = `${json.weather[0].description}`;
-        humidity.innerHTML = `${json.main.humidity}%`;
-        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
 
-        weatherBox.style.display = '';
-        weatherDetails.style.display = '';
-        weatherBox.classList.add('fadeIn');
-        weatherDetails.classList.add('fadeIn');
-        container.style.height = '590px';
-    });
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+        });
 });
+
+
